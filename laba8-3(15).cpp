9273b00da359d243d104d97c164f9c4b33abe959
Pavel-3 <75582049+Pavel-3@users.noreply.github.com>
2020-12-16 21:27:20 +0300

    Add files via upload

---
+++ laba8-3(15).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double solution(int n, int i = 0)
+{
+    if (i < n)
+        return 1 / (1 + solution(n, i + 1));
+    else
+        return 1;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int n;
+    cout << "Введите n: ";  cin >> n;
+    cout << solution(n);
+}
